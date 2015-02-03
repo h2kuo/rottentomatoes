d9705b0f12043f1538d737b64c11cee4c729add2
Helen Kuo <helenkuohk@gmail.com>
2015-02-02 20:13:40 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Helen Kuo on 2/2/15.
+//  Copyright (c) 2015 Helen Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Helen Kuo on 2/2/15.
+//  Copyright (c) 2015 Helen Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
